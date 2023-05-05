b96dad6b31c2ffb7735e8d3e8c90cfc970340c32
Aryann15 <rajlysm15@gmail.com>
2023-05-05 07:39:39 +0530

    basic structure for the to do app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="main_div">
+      <div className="center_div">
+        <br />
+        <h1> TO DO LIST</h1>
+        <br />
+        <input type="text" placeholder='Add a item' />
+        <button> + </button>
+        <ol>
+          <li> buy apple
+          </li>
+        </ol>
+      </div>
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
